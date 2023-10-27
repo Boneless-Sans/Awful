93be6068b2e57ec43f5d118f0aa7f7c00639f09a
Boneless-Sans <bonelessmail0101@gmail.com>
2023-10-27 12:36:46 -0700

    gpt hell

--- compile.bash
+++ compile.bash
@@ -1,27 +1,42 @@
 #!/bin/bash
 
 # Set the source directory where your Java files are located
-sourceDir="/Users/apcomputerscience/Desktop/Awful/src/"
+sourceDir="/Users/apcomputerscience/Desktop/Awful/src/java/"
 
 # Set the output directory where you want to store the main JAR file
-outputDir="/Users/apcomputerscience/Desktop/"
+outputDir="/Users/apcomputerscience/Desktop/output/"
 
 # Create a temporary directory for compiling and packaging
 tempDir=$(mktemp -d)
 
-# Compile all Java files and create the main JAR file
-for file in "$sourceDir"/*.java; do
-    # Extract the file name (without extension) from the path
-    baseName=$(basename "$file" .java)
+# Function to compile Java files
+compile_java() {
+    local source_file="$1"
+    local base_name="${source_file%.java}"
 
-    # Compile the Java file
-    javac -d "$tempDir" "$file"
+    # Create the directory structure for the class file
+    class_dir="${base_name#$sourceDir}"
+    class_dir="${class_dir//\//.}"
+
+    javac -d "$tempDir" "$source_file"
+}
+
+# Compile all Java files and create the main JAR file
+find "$sourceDir" -name "*.java" | while read source_file; do
+    compile_java "$source_file"
 done
 
+# Specify the main class with the full package and class name
+mainClass="src.java.Main"
+
 # Create the main JAR file
-jar cfe "$outputDir/main.jar" Launcher -C "$tempDir" .
+jar_file="$outputDir/Main.jar"
+jar cfe "$jar_file" "$mainClass" -C "$tempDir" .
 
 # Clean up the temporary directory
 rm -r "$tempDir"
 
 echo "Compilation and main JAR creation complete."
+
+# Run the JAR file
+java -jar "$jar_file"
--- src/java/Main.java
+++ src/java/Main.java
@@ -4,64 +4,242 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+import java.util.Enumeration;
+import java.util.jar.JarOutputStream;
 
-public class Main extends JFrame {
-    private JTextField classNameField;
-
-    public Main() {
-        setTitle("Java File Runner");
-        setSize(300, 120);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-
-        JPanel mainPanel = new JPanel();
-        mainPanel.setLayout(new BorderLayout());
-
-        classNameField = new JTextField();
-        JButton runButton = new JButton("Run");
-
-        mainPanel.add(classNameField, BorderLayout.CENTER);
-        mainPanel.add(runButton, BorderLayout.EAST);
-
-        runButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                runJavaFile();
-            }
-        });
-
-        add(mainPanel);
-    }
-
-    private void runJavaFile() {
-        String className = classNameField.getText();
-        if (className.isEmpty()) {
-            JOptionPane.showMessageDialog(this, "Please enter a class name.");
-            return;
-        }
-
-        String javaFileName = className + ".java";
-        File javaFile = new File(javaFileName);
-
-        if (!javaFile.exists()) {
-            JOptionPane.showMessageDialog(this, "Java file not found: " + javaFileName);
-            return;
-        }
-
-        try {
-            // Run the Java file using the system's default editor.
-            Desktop.getDesktop().edit(javaFile);
-        } catch (IOException e) {
-            e.printStackTrace();
-            JOptionPane.showMessageDialog(this, "An error occurred while opening the Java file.");
-        }
-    }
+public class Main {
+    private static JFrame errorFrame;
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            Main gui = new Main();
-            gui.setVisible(true);
+            JFrame frame = new JFrame("Application Launcher");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setSize(300, 150);
+            frame.setLayout(new FlowLayout());
+
+            // Create a list of application names by inspecting the JAR file
+            ArrayList<String> applications = getAvailableApplications();
+
+            JComboBox<String> appList = new JComboBox<>(applications.toArray(new String[0]));
+
+            JButton launchButton = new JButton("Launch");
+
+            launchButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    String selectedApp = (String) appList.getSelectedItem();
+                    if (selectedApp != null) {
+                        // Compile and run the selected application
+                        compileAndRun(selectedApp);
+                    }
+                }
+            });
+
+            frame.add(new JLabel("Select an application to launch:"));
+            frame.add(appList);
+            frame.add(launchButton);
+            frame.setVisible(true);
         });
+    }
+
+    private static void compileAndRun(String appName) {
+        try {
+            // Create a temporary directory for compilation and JAR creation
+            File tempDir = createTempDirectory();
+
+            // Compile the selected application
+            ProcessBuilder compileProcessBuilder = new ProcessBuilder("javac", "-d", tempDir.getAbsolutePath(), appName + ".java");
+            compileProcessBuilder.redirectErrorStream(true); // Redirect error stream to output
+            Process compileProcess = compileProcessBuilder.start();
+            int compilationResult = compileProcess.waitFor();
+
+            if (compilationResult == 0) {
+                // Compilation successful, create a JAR
+                createJar(tempDir, appName);
+
+                // Execute the created JAR
+                ProcessBuilder processBuilder = new ProcessBuilder("java", "-jar", appName + ".jar");
+                processBuilder.inheritIO();
+                processBuilder.directory(tempDir);
+                processBuilder.start();
+            } else {
+                // Compilation failed; display the error message
+                try (InputStream errorStream = compileProcess.getInputStream();
+                     InputStreamReader errorStreamReader = new InputStreamReader(errorStream);
+                     BufferedReader errorBufferedReader = new BufferedReader(errorStreamReader)) {
+                    StringBuilder errorMessage = new StringBuilder();
+                    String line;
+                    while ((line = errorBufferedReader.readLine()) != null) {
+                        errorMessage.append("Compilation error: ").append(line).append("\n");
+                    }
+
+                    // Show an error window with flashing "BUILD FAILED" message
+                    showBuildFailedWindow(errorMessage.toString());
+                }
+            }
+
+            // Clean up: delete the temporary directory
+            deleteTempDirectory(tempDir);
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void showBuildFailedWindow(String errorMessage) {
+        SwingUtilities.invokeLater(() -> {
+            errorFrame = new JFrame("Build Failed");
+            errorFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+            errorFrame.setSize(600, 100);
+            errorFrame.setResizable(false);
+
+            JPanel contentPanel = new JPanel(new BorderLayout()); // Create a panel for the entire content
+
+            // Set the background color for the content panel to red
+            contentPanel.setBackground(Color.RED);
+
+            JLabel buildFailedLabel = new JLabel("BUILD FAILED");
+            buildFailedLabel.setHorizontalAlignment(SwingConstants.CENTER);
+            buildFailedLabel.setForeground(Color.RED); // Initial text color red
+            buildFailedLabel.setFont(buildFailedLabel.getFont().deriveFont(Font.PLAIN, 18f));
+
+            JTextArea errorTextArea = new JTextArea(errorMessage);
+            errorTextArea.setEditable(false);
+            errorTextArea.setWrapStyleWord(true);
+            errorTextArea.setLineWrap(true);
+            errorTextArea.setForeground(Color.BLACK); // Set text color to black
+            errorTextArea.setFont(errorTextArea.getFont().deriveFont(Font.PLAIN, 14f));
+
+            contentPanel.add(buildFailedLabel, BorderLayout.NORTH);
+            contentPanel.add(errorTextArea, BorderLayout.CENTER);
+
+            errorFrame.add(contentPanel);
+
+            // Flashing white and red background
+            Timer timer = new Timer(500, new ActionListener() {
+                private boolean isRed = true;
+
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    if (isRed) {
+                        contentPanel.setBackground(Color.WHITE);
+                        buildFailedLabel.setForeground(Color.RED);
+                    } else {
+                        contentPanel.setBackground(Color.RED);
+                        buildFailedLabel.setForeground(Color.WHITE);
+                    }
+                    isRed = !isRed;
+                }
+            });
+
+            timer.start();
+
+            errorFrame.setVisible(true);
+        });
+    }
+
+
+
+
+
+
+    private static ArrayList<String> getAvailableApplications() {
+        ArrayList<String> appList = new ArrayList<>();
+        try {
+            // Get the package name based on the Main class's package
+            String packageName = Main.class.getPackage().getName();
+            JarFile jarFile = new JarFile(Main.class.getProtectionDomain().getCodeSource().getLocation().getPath());
+            Enumeration<JarEntry> entries = jarFile.entries();
+
+            while (entries.hasMoreElements()) {
+                JarEntry entry = entries.nextElement();
+                String entryName = entry.getName();
+                if (entryName.endsWith(".class") && entryName.startsWith(packageName.replace(".", "/")) && !entryName.equals(Main.class.getName().replace(".", "/") + ".class")) {
+                    String className = entryName.substring(0, entryName.length() - 6).replace("/", ".");
+                    appList.add(className);
+                }
+            }
+
+            jarFile.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return appList;
+    }
+
+    private static File createTempDirectory() throws IOException {
+        File tempDir = File.createTempFile("temp", Long.toString(System.nanoTime()));
+        if (!tempDir.delete() || !tempDir.mkdir()) {
+            throw new IOException("Could not create temporary directory: " + tempDir);
+        }
+        return tempDir;
+    }
+
+    private static void deleteTempDirectory(File tempDir) {
+        File[] files = tempDir.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                deleteTempDirectory(file);
+            }
+        }
+        tempDir.delete();
+    }
+
+    private static void createJar(File sourceDir, String appName) throws IOException {
+        File jarFile = new File(sourceDir, appName + ".jar");
+
+        try (JarOutputStream out = new JarOutputStream(new FileOutputStream(jarFile))) {
+            File[] files = sourceDir.listFiles();
+            if (files != null) {
+                for (File file : files) {
+                    addFileToJar(out, file, "");
+                }
+            }
+        }
+    }
+
+    private static void addFileToJar(JarOutputStream out, File file, String parent) {
+        try {
+            String entryName = parent + file.getName();
+            JarEntry entry = new JarEntry(entryName);
+
+            if (file.isDirectory()) {
+                entryName += "/";
+                entry.setMethod(JarEntry.STORED);
+                entry.setSize(0);
+                entry.setCrc(0);
+            } else {
+                entry.setMethod(JarEntry.DEFLATED);
+            }
+
+            out.putNextEntry(entry);
+
+            if (!file.isDirectory()) {
+                try (FileInputStream in = new FileInputStream(file)) {
+                    byte[] buffer = new byte[1024];
+                    int bytesRead;
+                    while ((bytesRead = in.read(buffer)) != -1) {
+                        out.write(buffer, 0, bytesRead);
+                    }
+                }
+            }
+
+            out.closeEntry();
+
+            if (file.isDirectory()) {
+                File[] files = file.listFiles();
+                if (files != null) {
+                    for (File nestedFile : files) {
+                        addFileToJar(out, nestedFile, entryName);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
