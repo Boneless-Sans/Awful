c266ddb81b63e745647c51483580c7ee82a508e0
Boneless-Sans <bonelessmail0101@gmail.com>
2023-11-18 11:52:14 -0800

    really hope gpt does not break this shit again

--- src/main/java/com/boneless/code/neighborhood/ColorPicker.java
+++ src/main/java/com/boneless/code/neighborhood/ColorPicker.java
@@ -2,50 +2,67 @@ package com.boneless.code.neighborhood;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class ColorPicker extends JFrame implements KeyListener {
-    private Color[] color = {
-            Color.red,
-            Color.orange,
-            Color.yellow,
-            Color.green,
-            Color.blue,
-            new Color(255,0,255),
-            new Color(139,69,19),
-            Color.black,
-            Color.white,
-            Color.gray,
-            Color.lightGray,
-            Color.darkGray
+public class ColorPicker extends JFrame{
+    private static final Color[] COLORS = {
+            Color.red, Color.orange, Color.yellow, Color.green, Color.cyan, Color.blue,
+            Color.pink, Color.magenta, new Color(139, 69, 19),
+            Color.white, Color.lightGray, Color.gray, Color.darkGray, Color.black
     };
-    public ColorPicker(){
-        setSize(500,500);
-        setTitle("Color Picker");
+    private static Color selectedColor;
+    private static JFrame frame;
 
-        System.out.println(color.length);
-        setVisible(true);
+    public static void showColorPicker() {
+        frame = new JFrame("Color Picker");
+        frame.setSize(500, 200);
+
+        JLabel title = new JLabel("Pick a Color");
+        title.setFont(new Font("Arial", Font.PLAIN, 15));
+
+        JPanel panel = new JPanel(new GridLayout(2, 6, 10, 10));
+
+        for(Color color : COLORS) {
+            JButton colorButton = new JButton();
+            colorButton.setBackground(color);
+            colorButton.setPreferredSize(new Dimension(10, 10));
+            System.out.println("uh");
+            frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+
+//            colorButton.addActionListener(e -> {
+//                selectedColor = color;
+//            });
+            colorButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+
+                }
+            });
+            panel.add(colorButton);
+        }
+        while (frame.isVisible()) {
+            try{
+                Thread.sleep(100);
+
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+
+        JButton exit = new JButton("Close");
+        exit.setFont(new Font("Arial", Font.PLAIN, 15));
+        exit.setFocusable(false);
+        exit.addActionListener(e -> {
+            frame.dispose();
+        });
+
+        frame.add(title, BorderLayout.NORTH);
+        frame.add(panel, BorderLayout.CENTER);
+        frame.add(exit, BorderLayout.SOUTH);
+        frame.setVisible(true);
     }
-    public void setColor(Color[] color){
-        this.color = color;
-    }
-    public Color getColor(){
-        return color[1];
-    }
-
-    @Override
-    public void keyTyped(KeyEvent e) {
-
-    }
-
-    @Override
-    public void keyPressed(KeyEvent e) {
-
-    }
-
-    @Override
-    public void keyReleased(KeyEvent e) {
-
+    public Color getSelectedColor(){
+        return selectedColor;
     }
 }
--- src/main/java/com/boneless/code/neighborhood/PainterPlus.java
+++ src/main/java/com/boneless/code/neighborhood/PainterPlus.java
@@ -1,63 +1,138 @@
 package com.boneless.code.neighborhood;
 
+import javax.swing.*;
 import java.awt.*;
+import java.awt.desktop.SystemEventListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.Objects;
 
 public class PainterPlus extends Painter implements KeyListener {
+    private static Color selectedColor;
+    private static boolean toggle = true;
+    private final int originalHeight = getHeight();
+    private final int originalWidth = getWidth();
+    private static final Color[] COLORS = {
+            Color.red, Color.orange, Color.yellow, Color.green, Color.cyan, Color.blue,
+            Color.pink, Color.magenta, new Color(139, 69, 19),
+            Color.white, Color.lightGray, Color.gray, Color.darkGray, Color.black
+    };
     public PainterPlus(){
         addKeyListener(this);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        JButton button = new JButton();
+        button.addActionListener(e -> {
+            colorPickerUI();
+        });
+    }
+    private void colorPickerUI() {
+        ColorPickerFrame frame = new ColorPickerFrame("Color Picker");
+        frame.setVisible(true);
+    }
+    private static class ColorPickerFrame extends JFrame {
+
+        public ColorPickerFrame(String title) {
+            super(title);
+            setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+            setSize(500,500);
+
+            JPanel panel = new JPanel(new GridLayout(2, 6, 5, 5));
+            for (Color color : COLORS) {
+                JButton colorButton = new JButton();
+                colorButton.setBackground(color);
+                colorButton.setPreferredSize(new Dimension(50, 50));
+                colorButton.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        selectedColor = color;
+                    }
+                });
+                panel.add(colorButton);
+            }
+
+            add(panel);
+
+            addWindowListener(new java.awt.event.WindowAdapter() {
+                @Override
+                public void windowClosing(java.awt.event.WindowEvent windowEvent) {
+                    // Custom logic when the close button is clicked
+                    // Dispose the frame and set toggle to false
+                    dispose();
+                    toggle = false;
+                }
+            });
+            setVisible(true);
+        }
     }
 
-    private Color color;
     @Override
     public void keyTyped(KeyEvent e) {
         switch(e.getKeyChar()){
             case'w':
-                while(!Objects.equals(getFacingDirection(), "north")){
-                    turnLeft();
+                if(toggle) {
+                    while (!Objects.equals(getFacingDirection(), "north")) {
+                        turnLeft();
+                    }
+                    move();
                 }
-                move();
                 break;
             case'd':
-                while(!Objects.equals(getFacingDirection(), "east")){
-                    turnLeft();
+                if(toggle) {
+                    while (!Objects.equals(getFacingDirection(), "east")) {
+                        turnLeft();
+                    }
+                    move();
                 }
-                move();
                 break;
             case's':
-                while(!Objects.equals(getFacingDirection(), "south")){
-                    turnLeft();
+                if(toggle) {
+                    while (!Objects.equals(getFacingDirection(), "south")) {
+                        turnLeft();
+                    }
+                    move();
                 }
-                move();
                 break;
             case'a':
-                while(!Objects.equals(getFacingDirection(), "west")){
-                    turnLeft();
+                if(toggle) {
+                    while (!Objects.equals(getFacingDirection(), "west")) {
+                        turnLeft();
+                    }
+                    move();
                 }
-                move();
                 break;
             case'e':
-                turnRight();
+                if(toggle) {
+                    turnRight();
+                }
                 break;
             case'q':
-                turnLeft();
+                if(toggle) {
+                    turnLeft();
+                }
                 break;
             case'c':
-                new ColorPicker();
+                if (toggle) {
+                    colorPickerUI();
+                    toggle = false;
+                } else {
+                    toggle = true;
+                }
                 break;
             case' ':
-                paint(color);
-                System.out.println("Painted");
+                if(toggle) {
+                    paint(selectedColor);
+                }
                 break;
         }
         if(e.getKeyChar() == KeyEvent.VK_ESCAPE){
-            System.exit(1);
+            if(toggle){
+                System.exit(0);
+            }
         }
     }
-
     @Override
     public void keyPressed(KeyEvent e) {
 
